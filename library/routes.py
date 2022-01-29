c9e8f193b2851743bf4140ee56cdafbba93b00ee
Tanmay Thole <tanmaythole@gmail.com>
2022-01-30 00:35:34 +0530

    import books from api created

--- library/routes.py
+++ library/routes.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from flask import redirect, request
 from .models import Books, Members
 from . import app, render_template, db
+import requests
 
 @app.route('/')
 def dashboard():
@@ -10,6 +12,65 @@ def dashboard():
 def books():
     books = Books.query.all()
     return render_template("books.html", books=books)
+
+@app.route('/books/import', methods=['POST', 'GET'])
+def import_books():
+    if request.method=='POST':
+
+        form = request.form
+        url = 'https://frappe.io/api/method/frappe-library'
+        params = {'page':1}
+
+        if form['title']:
+            params['title'] = form['title']
+        elif form['authors']:
+            params['authors'] = form['authors']
+        elif form['isbn']:
+            params['isbn'] = form['isbn']
+        elif form['publisher']:
+            params['publisher'] = form['publisher']
+
+        books_added = 0
+        
+        while books_added != int(form['no_of_books']):
+            res = requests.get(url, params=params).json()
+
+            if not res['message']:
+                break
+
+            for book in res['message']:
+                
+                if Books.query.filter_by(isbn=book['isbn']).first():
+                    continue
+                
+                b = Books(
+                        title=book['title'],
+                        authors=book['authors'],
+                        average_rating=book['average_rating'],
+                        isbn=book['isbn'],
+                        isbn13=book['isbn13'],
+                        language=book['language_code'],
+                        num_pages=book['  num_pages'],
+                        ratings_count=book['ratings_count'],
+                        pub_date=datetime.strptime(book['publication_date'], '%M/%d/%Y').date(),
+                        publisher=book['publisher'],
+                        quantity=form['quantity_per_books'],
+                        available_books=form['quantity_per_books'],
+                        issued_books=0
+                    )
+                db.session.add(b)
+                db.session.commit()
+                books_added += 1
+
+                if books_added == int(form['no_of_books']):
+                    break
+            
+            params['page'] += 1
+            
+        return redirect("/books")
+
+    return render_template("importBooks.html")
+
 
 @app.route('/books/add', methods=['POST', 'GET'])
 @app.route('/books/edit/<int:id>', methods=['POST', 'GET'])
