c3297a990d9d6f77e2e14ae71b9d32b89fdecda2
Tanmay Thole <tanmaythole@gmail.com>
2022-02-01 17:16:09 +0530

    flash messages added

--- library/__init__.py
+++ library/__init__.py
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tanmay:Pass1234@localho
 db = SQLAlchemy(app)
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 from . import routes
 from . import models
--- library/routes.py
+++ library/routes.py
@@ -1,15 +1,17 @@
 from datetime import datetime
-from flask import redirect, request
+from flask import flash, get_flashed_messages, redirect, request
 from .models import Books, Members, Transactions
 from . import app, render_template, db
 import requests
 
 @app.route('/')
 def dashboard():
+    get_flashed_messages()
     return render_template("dashboard.html")
 
 @app.route('/books')
 def books():
+    get_flashed_messages()
     books = Books.query.all()
     return render_template("books.html", books=books)
 
@@ -67,6 +69,13 @@ def import_books():
             
             params['page'] += 1
             
+        if books_added == int(form['no_of_books']):
+            flash(f"{books_added}/{form['no_of_books']} books added successfully.", 'success')
+        elif books_added==0:
+            flash(f"New books NOT found for given parameters", 'error')
+        else:
+            flash(f"{books_added}/{form['no_of_books']} books are added successfully.", 'warning')
+
         return redirect("/books")
 
     return render_template("importBooks.html")
@@ -75,105 +84,137 @@ def import_books():
 @app.route('/books/add', methods=['POST', 'GET'])
 @app.route('/books/edit/<int:id>', methods=['POST', 'GET'])
 def addBook(id=0):
-    if request.method=='POST':
-        if id==0:
-            # add book
-            r = request.form
-            b = Books(
-                    title=r['title'],
-                    authors = r['authors'],
-                    average_rating = r['average_rating'],
-                    isbn = r['isbn'],
-                    isbn13 = r['isbn13'],
-                    language = r['language'],
-                    num_pages = r['num_of_pages'],
-                    ratings_count = r['ratings_count'],
-                    pub_date = r['publication_date'],
-                    publisher = r['publisher'],
-                    quantity = r['quantity'],
-                    available_books = r['quantity'],
-                    issued_books = 0
-                )
-            db.session.add(b)
-            db.session.commit()
-            return redirect('/books')
-        else:
-            # edit book of id
-            r = request.form
-            b = Books.query.filter_by(id=id)
-            
-            b.update(dict(
-                    title=r['title'],
-                    authors = r['authors'],
-                    average_rating = r['average_rating'],
-                    isbn = r['isbn'],
-                    isbn13 = r['isbn13'],
-                    language = r['language'],
-                    num_pages = r['num_of_pages'],
-                    ratings_count = r['ratings_count'],
-                    pub_date = r['publication_date'],
-                    publisher = r['publisher'],
-                    quantity = r['quantity'],
-                    available_books = int(r['quantity']) - b[0].issued_books
-                ))
-            db.session.commit()
-            return redirect('/books')
-    
     if id==0:
         params = {"isNew":True}
     else:
         params = Books.query.filter_by(id=id).first()
-    
+
+
+    if request.method=='POST':
+        if id==0:
+            # add book
+            try:
+                r = request.form
+                b = Books(
+                        title=r['title'],
+                        authors = r['authors'],
+                        average_rating = r['average_rating'],
+                        isbn = r['isbn'],
+                        isbn13 = r['isbn13'],
+                        language = r['language'],
+                        num_pages = r['num_of_pages'],
+                        ratings_count = r['ratings_count'],
+                        pub_date = r['publication_date'],
+                        publisher = r['publisher'],
+                        quantity = r['quantity'],
+                        available_books = r['quantity'],
+                        issued_books = 0
+                    )
+                db.session.add(b)
+                db.session.commit()
+                flash(f'Book {b.title} Added Successfully!!', 'success')
+                return redirect('/books')
+            except Exception as e:
+                flash(f'Something went wrong while adding the book - {e}', 'error')
+                return render_template("addbook.html", params={params})
+        else:
+            # edit book of id
+            try:
+                r = request.form
+                b = Books.query.filter_by(id=id)
+
+                b.title=r['title']
+                b.authors = r['authors']
+                b.average_rating = r['average_rating']
+                b.isbn = r['isbn']
+                b.isbn13 = r['isbn13']
+                b.language = r['language']
+                b.num_pages = r['num_of_pages']
+                b.ratings_count = r['ratings_count']
+                b.pub_date = r['publication_date']
+                b.publisher = r['publisher']
+                b.quantity = r['quantity']
+                b.available_books = int(r['quantity']) - b[0].issued_books
+                
+                
+                db.session.commit()
+                
+                flash(f'Book {b.title} Updted Successfully!!', 'success')
+                return redirect('/books')
+            except Exception as e:
+                flash(f'Something went wrong while updating the book - {e}', 'error')
+                return render_template("addbook.html", params={params})
+
     return render_template("addbook.html", params=params)
 
 
 @app.route('/books/delete/<int:id>', methods=['DELETE', 'GET'])
 def deleteBook(id):
-    book = Books.query.filter_by(id=id).first()
-    db.session.delete(book)
-    db.session.commit()
+    try:
+        book = Books.query.filter_by(id=id).first()
+        db.session.delete(book)
+        db.session.commit()
+        flash(f"Book deleted Successfully.", "success")
+    except Exception as e:
+        flash(f"Something Went Wrong - {e}", "error")
     return redirect('/books')
 
 
 @app.route('/members')
 def members():
+    get_flashed_messages()
     members = Members.query.all()
     return render_template("members.html", members=members)
 
 @app.route('/members/add', methods=['GET', 'POST'])
 @app.route('/members/edit/<int:id>', methods=['GET', 'POST'])
 def addMember(id=0):
-    if request.method=='POST':
-        if id==0:
-            # add member
-            r = request.form
-            mem = Members(
-                    name = r['name'],
-                    email = r['email']
-                )
-            db.session.add(mem)
-            db.session.commit()
-            return redirect('/members')
-        else:
-            # edit member of id
-            r = request.form
-            Members.query.filter_by(id=id).update(dict(name=r['name'], email=r['email']))
-            db.session.commit()
-            return redirect('/members')
-    
+
     if id==0:
         params = {"isNew":True}
     else:
         params = Members.query.filter_by(id=id)[0]
     
+    if request.method=='POST':
+        if id==0:
+            # add member
+            try:
+                r = request.form
+                mem = Members(
+                        name = r['name'],
+                        email = r['email']
+                    )
+                db.session.add(mem)
+                db.session.commit()
+                flash(f"Member {mem.name} Added Successfully!!", "success")
+                return redirect('/members')
+            except Exception as e:
+                flash(f'Something went wrong while adding the member - {e}', 'error')
+                return render_template("addmember.html", params={params})
+        else:
+            # edit member of id
+            try:
+                r = request.form
+                Members.query.filter_by(id=id).update(dict(name=r['name'], email=r['email']))
+                db.session.commit()
+                flash(f"Member {id} Updated Successfully!!", "success")
+                return redirect('/members')
+            except Exception as e:
+                flash(f'Something went wrong while updating the member - {e}', 'error')
+                return render_template("addmember.html", params={params})
+                
     return render_template('addmember.html', params=params)
 
 
 @app.route('/members/delete/<int:id>', methods=['DELETE', 'GET'])
 def deleteMember(id):
-    mem = Members.query.filter_by(id=id).first()
-    db.session.delete(mem)
-    db.session.commit()
+    try:
+        mem = Members.query.filter_by(id=id).first()
+        db.session.delete(mem)
+        db.session.commit()
+        flash(f"Member Deleted Successfully.", "success")
+    except Exception as e:
+        flash(f"Something Went Wrong - {e}", "error")
     return redirect('/members')
 
 @app.route('/transactions')
@@ -202,10 +243,12 @@ def issueBook():
                 isBookFound.issued_books=isBookFound.issued_books+1
 
                 db.session.commit()
+
+                flash(f"Book issued successfully.", "success")
             else:
-                print("Books Not Available")
+                flash(f"Book with {form['book']} id not available.", "error")
         else:
-            print(f"Book not found to {form['book']} id.")
+            flash(f"Book not found to {form['book']} id.", "error")
         return redirect('/transactions')
     return render_template('issueBook.html')
 
@@ -214,6 +257,7 @@ def returnBook(id):
     transaction = Transactions.query.filter_by(id=id).first()
 
     if transaction is None:
+        flash(f"Something Went Wrong.", 'error')
         return redirect('/transactions')
 
     book = Books.query.filter_by(id=transaction.book_id).first()
@@ -225,28 +269,32 @@ def returnBook(id):
     total_charge = total_no_of_days * transaction.per_day_fee
 
     if request.method=='POST':
-        form = request.form
+        try:
+            form = request.form
 
-        debt = total_charge - int(form['amount_paid'])
+            debt = total_charge - int(form['amount_paid'])
 
-        if member.outstanding_debt + debt < 500:
-            transaction.returned_on = datetime.now()
-            transaction.total_charge = total_charge
-            transaction.amount_paid = int(form['amount_paid'])
-            transaction.isClosed = True
-            db.session.commit()
+            if member.outstanding_debt + debt < 500:
+                transaction.returned_on = datetime.now()
+                transaction.total_charge = total_charge
+                transaction.amount_paid = int(form['amount_paid'])
+                transaction.isClosed = True
+                db.session.commit()
 
-            member.outstanding_debt += debt
-            member.amount_spent += int(form['amount_paid'])
-            db.session.commit()
+                member.outstanding_debt += debt
+                member.amount_spent += int(form['amount_paid'])
+                db.session.commit()
 
-            book.available_books += 1
-            book.issued_books -= 1
-            db.session.commit()
+                book.available_books += 1
+                book.issued_books -= 1
+                db.session.commit()
 
-            return redirect('/transactions')
-        else:
-            print('OutStanding Debt is more than 500')
+                flash(f"Book returned successfully.", "success")
+                return redirect('/transactions')
+            else:
+                flash(f"OutStanding Debt of member is more than 500.", "error")
+        except Exception as e:
+            flash(f"Something Went Wrong.", "error")
 
 
     return render_template("returnBook.html", book=book, transaction=transaction, member=member, params={"total_no_of_days":total_no_of_days, "total_charge":total_charge})
